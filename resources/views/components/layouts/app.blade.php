ae532391cfe494708415ddf8c9032a77cc7e46ca
Archie <archiealvarado20@gmail.com>
2024-12-07 22:36:24 +0800

    first commit

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link
+      href="https://cdn.jsdelivr.net/npm/remixicon@4.5.0/fonts/remixicon.css"
+      rel="stylesheet"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"
+    />
+        <title>{{ $title ?? 'Page Title' }}</title>
+        @vite(['resources/css/app.css','resources/css/styles.css','resources/js/app.js' ,'resources/js/main.js'])
+        @livewireStyles
+    </head>
+    <body>
+    @livewire('partials.navbar')
+    <main>
+    {{ $slot }}
+    </main>
+    @livewire('partials.footer')
+    </body>
+    
+    <script src="https://unpkg.com/scrollreveal"></script>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+</html>
---
+++ resources/views/livewire/partials/navbar.blade.php
@@ -0,0 +1,27 @@
+<div>
+<nav>
+        <div class="nav__header">
+          <div class="nav__logo">
+            <a href="#" class="logo">
+              <img src="{{url('assets/logo1.png')}}" alt="logo" class="logo-white" />
+              <img src="assets/logo1.png" alt="logo" class="logo-dark" />
+              <span> Franck Rentals</span>
+            </a>
+          </div>
+          <div class="nav__menu__btn" id="menu-btn">
+            <i class="ri-menu-line"></i>
+          </div>
+        </div>
+        <ul class="nav__links" id="nav-links">
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#deals">Rental Deals</a></li>
+          <li><a href="#choose">Why Choose Us</a></li>
+          <li><a href="#client">Testimonials</a></li>
+          <li><a href="#">Register</a></li>
+        </ul>
+        <div class="nav__btns">
+          <button class="btn">Register</button>
+        </div>
+      </nav>
+</div>
